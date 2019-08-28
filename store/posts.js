5bd8d247a7ce77fe4289b296c028c2930a2e179d
mutohiroshi <mutohiroshi@team-lab.com>
2019-08-28 11:24:17 +0900

    nuxt training

---
+++ store/posts.js
@@ -0,0 +1,24 @@
+export const state = () => ({
+    all: [
+        {
+            id: 'balut',
+            title: 'what is Bault?',
+            content: 'aaaaa'
+        },
+        {
+            id: 'emet',
+            title: 'what is Emet?',
+            content: 'bbbbb'
+        },
+        {
+            id: 'text',
+            title: 'what is text?',
+            content: 'ccc'
+        },
+        {
+            id: 'title',
+            title: 'what is title?',
+            content: 'dddd'
+        },
+    ]
+})
